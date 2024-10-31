546c7c164249c57cfe9d053f8420dc4baa727189
Mari <maryadrielle@hotmail.com>
2024-10-31 17:01:16 -0300

    feat: Add database conection

---
+++ Data/BookstoreContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Bookstoret2.Models;
+
+namespace Bookstoret2.Data
+{
+	public class BookstoreContext : DbContext
+	{
+		public BookstoreContext(DbContextOptions<BookstoreContext> options) : base(options)
+		{
+		}
+
+        public DbSet<Genre> Genres { get; set; }
+    }
+}
