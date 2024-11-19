38af6b2ee3972fd6c7383bc2c2e9aff88aff8645
Mari <maryadrielle@hotmail.com>
2024-11-19 16:50:54 -0300

    feat: Add genre edit feature

---
+++ Services/Exceptions/DbConcurrencyException.cs
@@ -0,0 +1,9 @@
+﻿namespace Bookstoret2.Services.Exceptions
+{
+	public class DbConcurrencyException : ApplicationException
+	{
+		public DbConcurrencyException(string? message) : base(message)
+		{
+		}
+	}
+}
