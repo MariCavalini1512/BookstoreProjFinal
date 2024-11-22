ad1f3aeddab762b1e8bcb7934cd59008b63c839f
Mari <maryadrielle@hotmail.com>
2024-11-22 16:34:26 -0300

    feat: Add book model

--- Migrations/20241121200019_Other-Entities.Designer.cs
+++ Migrations/20241121200019_Other-Entities.Designer.cs
@@ -3,6 +3,7 @@ using Bookstoret2.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Bookstoret2.Migrations
 {
     [DbContext(typeof(BookstoreContext))]
-    partial class BookstoreContextModelSnapshot : ModelSnapshot
+    [Migration("20241121200019_Other-Entities")]
+    partial class OtherEntities
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Models/Book.cs
@@ -0,0 +1,44 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Bookstoret2.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Título")]
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        public string Title { get; set; }
+
+        [Display(Name = "Preço")]
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [DataType(DataType.Currency)]
+        [DisplayFormat(DataFormatString = "{0:C2}")]
+        public double Price { get; set; }
+
+        [Display(Name = "Autor")]
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        public string Author { get; set; }
+
+        [Display(Name = "Ano de Lançamento")]
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        public int ReleaseYear { get; set; }
+
+        [Display(Name = "Gêneros Literários")]
+        public ICollection<Genre> Genres { get; set; } = new List<Genre>();
+
+        public Book()
+        {
+
+        }
+
+        public Book(int id, string title, double price, string author, int releaseYear)
+        {
+            Id = id;
+            Title = title;
+            Price = price;
+            Author = author;
+            ReleaseYear = releaseYear;
+        }
+    }
+}
